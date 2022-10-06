a3c88668986ad85be6aa6240b5826927f2e85f8f
[Maureen Bayne] <[maureenbayne34@gmail.com]>
2022-10-06 15:52:07 -0600

    initial files for snake game

---
+++ scoreboard.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 270)
+        self.color("#3b5d05")
+        self.write(f"Score: {self.score}", move=False, align='center', font=('Impact', 15, 'normal'))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", move=False, align='center', font=('Impact', 15, 'normal'))
